3a01d971a2e355a5510f10e8cea84a065a08d5b1
chukwukai14 <chukwukai2007@gmail.com>
2024-11-04 21:03:38 +0100

    (week 3 commit)

---
+++ week-3/practice_3/target/debug/practice_3.d
@@ -0,0 +1,1 @@
+C:\Users\DELL\ LATITUDE\ 7490\Documents\c.nnodimCSC101\week-3\practice_3\target\debug\practice_3.exe: C:\Users\DELL\ LATITUDE\ 7490\Documents\c.nnodimCSC101\week-3\practice_3\src\main.rs
---
+++ week-3/practice_4/target/debug/practice_4.d
@@ -0,0 +1,1 @@
+C:\Users\DELL\ LATITUDE\ 7490\Documents\c.nnodimCSC101\week-3\practice_4\target\debug\practice_4.exe: C:\Users\DELL\ LATITUDE\ 7490\Documents\c.nnodimCSC101\week-3\practice_4\src\main.rs

659b2fc0d448b0d8f90acf3f2fa3159c736affeb
chukwukai14 <chukwukai2007@gmail.com>
2025-01-02 13:54:21 +0100

    my week 6 commit

---
+++ week-6/practice_1/target/debug/practice_1.d
@@ -0,0 +1,1 @@
+C:\Users\DELL\ LATITUDE\ 7490\Documents\c.nnodimCSC101\week-6\practice_1\target\debug\practice_1.exe: C:\Users\DELL\ LATITUDE\ 7490\Documents\c.nnodimCSC101\week-6\practice_1\src\main.rs
---
+++ week-6/practice_3/target/debug/practice_3.d
@@ -0,0 +1,1 @@
+C:\Users\DELL\ LATITUDE\ 7490\Documents\c.nnodimCSC101\week-6\practice_3\target\debug\practice_3.exe: C:\Users\DELL\ LATITUDE\ 7490\Documents\c.nnodimCSC101\week-6\practice_3\src\main.rs
